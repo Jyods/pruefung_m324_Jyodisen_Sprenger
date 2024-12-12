3643923f764458cc6359a67d66fa77ebc030dcce
Jyods <jyodi.sprenger@hotmail.ch>
2024-12-12 09:24:47 +0100

    Revert "formatter test"
    This reverts commit b83cb9f771a6e5b076587f6fc3d27deaa59246fd.

--- eslint.config.js
+++ eslint.config.js
@@ -1,15 +1,7 @@
-module.exports =
-  {
-    rules:
-      {
-        'no-unused-vars':
-          'error',
-        'prefer-const':
-          'error',
-        eqeqeq:
-          [
-            'error',
-            'always',
-          ],
-      },
-  };
+module.exports = {
+    "rules": {
+        "no-unused-vars": "error",
+        "prefer-const": "error",
+        "eqeqeq": ["error", "always"]
+    }
+};
--- index.js
+++ index.js
@@ -1,19 +1,9 @@
-import chalk from 'chalk';
+import chalk from "chalk";
 
-console.log(
-  chalk.blue(
-    'It works',
-  ),
-);
+console.log(chalk.blue("It works"));
 
-const myUnusedVariable =
-  "I'm not used";
+const myUnusedVariable = "I'm not used";
 
-if (
-  myUnusedVariable ===
-  "I'm not used"
-) {
-  console.log(
-    'This is always true',
-  );
+if (myUnusedVariable === "I'm not used") {
+  console.log("This is always true");
 }
